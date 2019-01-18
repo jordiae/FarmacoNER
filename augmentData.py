422950ea0be83cad211cbefe0bd3641189cc6590
Jordi Armengol <me@jordiae.com>
2019-01-18 14:02:13 +0100

    augmentData final

--- augmentData.py
+++ augmentData.py
@@ -6,7 +6,8 @@ MRCONSO = dataset.connect('sqlite:///../mrconso.db')
 MRSTY = dataset.connect('sqlite:///../mrsty.db')
 
 XML_PATH = '../EC21.d/'
-XML_FILE = XML_PATH + 'EMEA_es_EC21_man.xml'
+XML_FILE_EMEA = XML_PATH + 'EMEA_es_EC21_man.xml'
+XML_FILE_MEDLINE = XML_PATH + 'Medline_es_EC21_man.xml'
 
 Concept = collections.namedtuple('Concept',['cui','len','text','offset'])
 Sentence = collections.namedtuple('Sentence',['id','text','concepts'])
@@ -19,9 +20,9 @@ T1  NORMALIZABLES 3688 3692 CDDP
 '''
 AUGMENTED_DATA_PATH = '../augmentedData/'
 
-def getAllSentencesFromXML():
-    print('Parsing XML',XML_FILE,'...')
-    tree = ET.parse(XML_FILE)
+def getAllSentencesFromXML(filePath):
+    print('Parsing XML',filePath,'...')
+    tree = ET.parse(filePath)
     print('Processing XML...')
     root = tree.getroot()
     sentences = [] # array of: [title,text,[concenpts]]
@@ -54,9 +55,11 @@ def getAllSentencesFromXML():
             sentence = Sentence(id = unit.attrib['id'], text = unit[0].text, concepts = concepts)
             sentences.append(sentence)
             unitIn += 1
+            '''
             if (unitIn > maxUnits):
                 break
-        break
+            '''
+        #break
     return sentences
 
 def createAnnotation(T,label,offset,length,name,numNote,cui):
@@ -67,16 +70,17 @@ def createAnnotationSet(sentence):
     # Concept = collections.namedtuple('Concept',['cui','len','text','offset'])
     # MRCONSO, MRSTY
     uniqueConcepts = []
-    currentConceptText = sentence.concepts[0].text
+    currentConceptOffset = sentence.concepts[0].offset#currentConceptText = sentence.concepts[0].text
     #uniqueConcepts.append(sentence.concepts[0].text,[sentence.concepts[0]])
-    uniqueConcepts = {sentence.concepts[0].text:[sentence.concepts[0]]}
+    #uniqueConcepts = {sentence.concepts[0].text:[sentence.concepts[0]]}
+    uniqueConcepts = {sentence.concepts[0].offset:[sentence.concepts[0]]}
     for concept in sentence.concepts:
-        if concept.text != currentConceptText:
+        if concept.offset != currentConceptOffset:#if concept.text != currentConceptText:
             #uniqueConcepts.append(concept.text,[])
-            uniqueConcepts[concept.text] = [concept]
-            currentConceptText = concept.text
+            uniqueConcepts[concept.offset] = [concept]#uniqueConcepts[concept.text] = [concept]
+            currentConceptOffset = concept.offset #currentConceptText = concept.text
         else:
-            uniqueConcepts[currentConceptText].append(concept)
+            uniqueConcepts[currentConceptOffset].append(concept)#uniqueConcepts[currentConceptText].append(concept)
 
     # compare semantic types etc
     '''
@@ -96,29 +100,45 @@ def createAnnotationSet(sentence):
     annotations = []
     numNotes = 0
     for uniqueConcept, concepts in uniqueConcepts.items():
-        ts = []
+        ts = set([])
         for concept in concepts:
-            row = MRSTY['mrconso'].find_one(cui = concept.cui) # the table is named mrconso instead of mrsty
-            ts.append(row['ts'])
-        ts = set(ts)
-        print()
-        print(ts)
+            #row = MRSTY['mrconso'].find_one(cui = concept.cui) # the table is named mrconso instead of mrsty
+            rows = MRSTY['mrconso'].find(cui = concept.cui)
+            if rows != None:
+                empty = True
+                for tt in rows:
+                    empty = False
+                    #ts.append(tt['ts'])
+                    ts.add(tt['ts'])
+                if empty:
+                    print('Warning: CUI', concept.cui, 'was not found in UMLS')
+            else:
+                print('Warning: CUI', concept.cui, 'was not found in UMLS')
+        #ts = set(ts)
+        #print()
+        #print(ts)
         label = ''
         if len(ts.intersection(proteinsTs)) > 0 and len(ts.intersection(chemicalTs)) == 0:
             label = 'PROTEINAS'
         elif len(ts.intersection(proteinsTs)) == 0 and len(ts.intersection(chemicalTs)) > 0:
-            row2 = MRCONSO['mrconso'].find_one(cui = concept.cui)
+            row2 = MRCONSO['mrconso'].find_one(cui = concept.cui, source='SNOMEDCT_US')
+            if row2 != None:
+                label = 'NORMALIZABLES'
+            else:
+                label = 'NO_NORMALIZABLES'
+            '''
             if row2['source'] == 'SNOMEDCT_US':
                 label = 'NORMALIZABLES'
             else:
                 label = 'NO_NORMALIZABLES'
+            '''
         else:
-            print('WHAT TO DO HERE)')
+            #print('WHAT TO DO HERE)')
             label = 'OTHER'
-        print(label)
+        #print(label)
         annotations.append(createAnnotation(T = numNotes+1,label = label,offset = uniqueConcepts[uniqueConcept][0].offset,\
-            length = uniqueConcepts[uniqueConcept][0].len,name = uniqueConcept,numNote = numNotes+1,cui=uniqueConcepts[uniqueConcept][0].cui)) # T? cui?
-        print()
+            length = uniqueConcepts[uniqueConcept][0].len,name = uniqueConcepts[uniqueConcept][0].text,numNote = numNotes+1,cui=uniqueConcepts[uniqueConcept][0].cui)) # T? cui?
+       #print()
         numNotes += 1
     return AnnotationSet(annotations = annotations)
 
@@ -143,6 +163,7 @@ def writePair(sentence,annotationSet):
         #print(ann)
     #print()
     #return None
+    '''
     print(sentence.id + '.txt:')
     print(txt)
     print()
@@ -150,27 +171,32 @@ def writePair(sentence,annotationSet):
     print(ann)
     print()
     print()
+    '''
+    #print('Adding sentence ' + sentence.id)
+    with open(AUGMENTED_DATA_PATH + sentence.id + '.txt','w') as f:
+        f.write(txt)
+    with open(AUGMENTED_DATA_PATH + sentence.id + '.ann','w') as f:
+        f.write(ann)
 
 def main():
-    sentences = getAllSentencesFromXML()
-    for sentence in sentences:
-        print(sentence)
-        print()
-        print()
-        '''
-        print('TITLE',sentence[0])
-        print('TEXT',sentence[1])
-        print('ES')
-        for concepts in sentence[2]:
-            #print('CUI',concepts[0],'len',concepts[1],'text', "'"+concepts[2]+"'")
-            print()
-        print()
-        print()
-        '''
-    annotations = []
-    for sentence in sentences:
+    sentences = getAllSentencesFromXML(XML_FILE_EMEA)
+    #annotations = []
+    for index,sentence in enumerate(sentences):
+        print('Processing and writing sentence ', sentence.id, ' (',index,'/',len(sentences),') of', XML_FILE_EMEA)
+        writePair(sentence,createAnnotationSet(sentence))
+
+    sentences = getAllSentencesFromXML(XML_FILE_MEDLINE)
+    for index,sentence in enumerate(sentences):
+        print('Processing and writing sentence ', sentence.id, ' (',index,'/',len(sentences),') of', XML_FILE_MEDLINE)
+        writePair(sentence,createAnnotationSet(sentence))
+        
+    '''
+    for index,sentence in enumerate(sentences):
+        print('Processing sentence ', sentence.id, ' (',index,'/',len(sentences),') of', XML_FILE_EMEA)
         annotations.append(createAnnotationSet(sentence))
     for sentence,annotationSet in zip(sentences,annotations):
+        print('Writing sentence', sentence.id)
         writePair(sentence,annotationSet)
+    '''
 if __name__ == "__main__":
     main()
